108263255cdca8a548369702d6f69be8dfabc0cc
Satoshi Nishimura <nishim314@gmail.com>
2011-10-07 17:16:17 +0900

    adding files for view of plain sql.

---
+++ Factory/View.php
@@ -0,0 +1,35 @@
+<?php
+
+class YokazeDb_Factory_View
+{
+    private $config = array();
+
+    public function __construct($config)
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * Return iterator.
+     *
+     * @param string $sqlFile
+     * @return Laiz_Db_Iterator_View
+     */
+    public function create($sqlFile)
+    {
+        try {
+            require_once dirname(dirname(__FILE__)) . '/Driver/Factory.php';
+            $db = YokazeDb_Driver_Factory::factory($this->config['dsn']);
+        }catch (PDOException $e){
+            // PDO error
+            trigger_error($e->getMessage(), E_USER_ERROR);
+        }catch (Exception $e){
+            // framework error
+            trigger_error($e->getMessage(), E_USER_ERROR);
+        }
+        require_once dirname(dirname(__FILE__)) . '/View.php';
+        $dao = new YokazeDb_View($db);
+        require_once dirname(dirname(__FILE__)) . '/Iterator/View.php';
+        return new YokazeDb_Iterator_View($dao, $sqlFile);
+    }
+}
---
+++ Iterator/View.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * Simple O/R Mapper Iterator Class File
+ *
+ * PHP versions 5
+ *
+ * @author    Satoshi Nishimura <nishim314@gmail.com>
+ * @copyright 2009-2011 Satoshi Nishimura
+ */
+
+require_once dirname(dirname(__FILE__)) . '/Vo.php';
+
+/**
+ * Simple O/R Mapper Iterator Class
+ *
+ * @author    Satoshi Nishimura <nishim314@gmail.com>
+ */
+class YokazeDb_Iterator_View implements Iterator
+{
+    protected $dao;
+    protected $sqlFile;
+    protected $params;
+    protected $replacements;
+    protected $key;
+    protected $vo;
+    protected $isContinue;
+
+    public function __construct(YokazeDb_View $dao, $sqlFile)
+    {
+        $this->sqlFile = $sqlFile;
+        $this->setVo($dao->createVo($sqlFile));
+        $this->dao = $dao;
+    }
+
+    protected function setVo(YokazeDb_Vo $vo)
+    {
+        $this->vo = $vo;
+    }
+
+    /**
+     * Set arguments of prepared statement.
+     *
+     * @param array $params
+     */
+    public function setParams($params)
+    {
+        $this->params = $params;
+        return $this;
+    }
+
+    /**
+     * Format string of replacement SQL.
+     *
+     * @param string $file
+     */
+    public function setReplacements($reps)
+    {
+        $this->replacements = $reps;
+        return $this;
+    }
+
+    protected function getStatement(){
+        return $this->dao->prepareStmt($this->sqlFile, $this->params, $this->replacements);
+    }
+
+    protected function bind(PDOStatement $stmt, $vo){
+        $this->dao->bind($stmt, $vo);
+    }
+
+    public function rewind(){
+        $this->stmt = $this->getStatement();
+        if (!$this->stmt instanceof PDOStatement){
+            trigger_error('Can not get PDOStatement.', E_USER_WARNING);
+            return;
+        }
+
+        if (!is_object($this->vo))
+            $this->vo = new StdClass;
+        $this->bind($this->stmt, $this->vo);
+        $this->key = 0;
+        $this->isContinue = $this->stmt->fetch(PDO::FETCH_BOUND);
+    }
+
+    public function current()
+    {
+        return $this->vo;
+    }
+
+    public function key(){
+        return $this->key;
+    }
+
+    public function next(){
+        $this->isContinue = $this->stmt->fetch(PDO::FETCH_BOUND);
+    }
+
+    public function valid(){
+        return $this->isContinue;;
+    }
+
+    public function count()
+    {
+        $stmt = $this->getStatement();
+        return $stmt->rowCount();
+        // Warning: This function was checked PostgreSQL only.
+    }
+        
+}
